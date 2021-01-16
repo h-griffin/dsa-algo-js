19ecdc94e9b9545ad7561bed37c60954d80c030a
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-01-15 21:49:24 -0800

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,28 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+// from 0 to n (itterate through rows)
+//     create an empty string 'level'
+//     rom 0 to ? (columns)
+//         if the colun should have "#"
+//             add "#" to 'level'
+//         else 
+//             add a space to level 
+//     console.log level
+
+function pyramid(n) {
+    const midpoint = Math.floor(2*n -1 /2);
+
+    for (let row = 0; row < n; row++){
+        let level = '';
+        
+        for(let column = 0; columns = 2*n -1; column++ ){
+            if (midpoint - row <= column && midpoint + row >= column ){
+                level += '#';
+            }
+        }
+    }
+}
 
 module.exports = pyramid;
